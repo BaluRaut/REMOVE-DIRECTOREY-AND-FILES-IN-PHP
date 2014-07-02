0c9da59d6e467aafb3db7295fbe4b703cb8146c8
Balasaheb <balsraut@gmail.com>
2014-07-02 13:16:31 +0530

    Create remove.php
    Please let me know if you have any help Email: braut@avika.in

---
+++ remove.php
@@ -0,0 +1,33 @@
+<?php
+
+
+function remove_dir_and_files($dir) {
+
+    $dir_path = $_SERVER['DOCUMENT_ROOT'].'/';
+    
+	// Please echo it and see what is going to deleted      echo $root.$dir; die(); 
+	
+  // This is the condition 
+    
+	if (is_dir($dir_path.$dir)) {
+      
+      // This is the command and default function
+       exec('rm -rf '.$dir_path.$dir);
+       
+	      echo $dir_path.$dir."    Successfully Deleted <br/>";
+	      
+	     }
+	else	   {
+	
+	    echo "This is not Directory, You can only delete the directory.";
+	    
+	   }
+}
+
+
+// Now call the functions with $directory name
+
+remove("wp-content/uploads/");
+
+
+?>
